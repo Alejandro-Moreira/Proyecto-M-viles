9c6c97b2ef8555d5d30bed15b84bd82916ba7b3b
Alejandro-Moreira <moreiramikel11@gmail.com>
2024-08-04 17:12:06 -0500

    Clases Java

--- app/src/main/java/com/example/maprealtime/ui/Params.java
+++ app/src/main/java/com/example/maprealtime/ui/Params.java
@@ -1,4 +1,15 @@
 package com.example.maprealtime.ui;
 
+import android.app.Activity;
+import android.content.Context;
+
+import java.util.ArrayList;
+
 public class Params {
+    public static String UserFirebaseId;
+    public static String userId;
+    public static Activity activityService;
+    public static Context contextService;
+    public static Mineros useerApp;
+    public static ArrayList<Users> userCiclistasAll;
 }
