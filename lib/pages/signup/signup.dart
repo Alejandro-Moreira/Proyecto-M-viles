9003318b12ced652be2cb6739d718c2368e22712
Alejandro Moreira <117743484+Alejandro-Moreira@users.noreply.github.com>
2024-08-09 16:03:52 -0500

    Estilos

--- lib/pages/signup/signup.dart
+++ lib/pages/signup/signup.dart
@@ -8,7 +8,6 @@ class Signup extends StatefulWidget {
   const Signup({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _SignupState createState() => _SignupState();
 }
 
@@ -22,7 +21,6 @@ class _SignupState extends State<Signup> {
     return Scaffold(
       backgroundColor: Colors.white,
       resizeToAvoidBottomInset: true,
-      bottomNavigationBar: _signin(context),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -32,41 +30,82 @@ class _SignupState extends State<Signup> {
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Center(
-                child: Text(
-                  'Regístrate',
-                  style: GoogleFonts.raleway(
-                    textStyle: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 32,
+                child: Column(
+                  children: [
+                    Text(
+                      'MapRealTime',
+                      style: GoogleFonts.raleway(
+                        textStyle: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                        ),
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Regístrate',
+                      style: GoogleFonts.raleway(
+                        textStyle: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 80,),
-              _emailAddress(),
-              const SizedBox(height: 20,),
-              _password(),
-              const SizedBox(height: 20,),
-              _roleDropdown(),
-              const SizedBox(height: 50,),
-              _signup(context),
+              const SizedBox(height: 40),
+              _buildTextField(
+                controller: _emailController,
+                label: 'Correo electrónico',
+                hintText: 'Ingrese su correo electrónico',
+              ),
+              const SizedBox(height: 20),
+              _buildTextField(
+                controller: _passwordController,
+                label: 'Contraseña',
+                hintText: 'Ingrese su contraseña',
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              _buildDropdown(),
+              const SizedBox(height: 30),
+              _buildButton(
+                context: context,
+                text: 'Regístrate',
+                onPressed: () async {
+                  await AuthService().signup(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context,
+                    role: _selectedRole,
+                  );
+                },
+              ),
             ],
           ),
         ),
       ),
+      bottomNavigationBar: _signin(context),
     );
   }
 
-  Widget _emailAddress() {
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required String hintText,
+    bool obscureText = false,
+  }) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'Correo electrónico',
+          label,
           style: GoogleFonts.raleway(
             textStyle: const TextStyle(
               color: Colors.black,
@@ -75,21 +114,22 @@ class _SignupState extends State<Signup> {
             ),
           ),
         ),
-        const SizedBox(height: 16,),
+        const SizedBox(height: 8),
         TextField(
-          controller: _emailController,
+          controller: controller,
+          obscureText: obscureText,
           decoration: InputDecoration(
             filled: true,
-            hintText: 'Ingrese su correo electrónico',
-            hintStyle: const TextStyle(
-              color: Color(0xff6A6A6A),
-              fontWeight: FontWeight.normal,
-              fontSize: 14,
-            ),
             fillColor: const Color(0xffF7F7F9),
             border: OutlineInputBorder(
               borderSide: BorderSide.none,
               borderRadius: BorderRadius.circular(14),
+            ),
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              color: Color(0xff6A6A6A),
+              fontWeight: FontWeight.normal,
+              fontSize: 14,
             ),
           ),
         ),
@@ -97,45 +137,7 @@ class _SignupState extends State<Signup> {
     );
   }
 
-  Widget _password() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Contraseña',
-          style: GoogleFonts.raleway(
-            textStyle: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.normal,
-              fontSize: 16,
-            ),
-          ),
-        ),
-        const SizedBox(height: 16,),
-        TextField(
-          controller: _passwordController,
-          obscureText: true,
-          decoration: InputDecoration(
-            hintText: 'Ingrese su contraseña',
-            hintStyle: const TextStyle(
-              color: Color(0xff6A6A6A),
-              fontWeight: FontWeight.normal,
-              fontSize: 14,
-            ),
-            filled: true,
-            fillColor: const Color(0xffF7F7F9),
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(14),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _roleDropdown() {
+  Widget _buildDropdown() {
     return DropdownButton<String>(
       value: _selectedRole,
       items: <String>['Usuario', 'Administrador'].map((String value) {
@@ -149,28 +151,33 @@ class _SignupState extends State<Signup> {
           _selectedRole = newValue!;
         });
       },
+      style: GoogleFonts.raleway(
+        textStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+        ),
+      ),
     );
   }
 
-  Widget _signup(BuildContext context) {
+  Widget _buildButton({
+    required BuildContext context,
+    required String text,
+    required VoidCallback onPressed,
+    Color color = const Color(0xff0D6EFD),
+  }) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xff0D6EFD),
+        backgroundColor: color,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(14),
         ),
         minimumSize: const Size(double.infinity, 60),
         elevation: 0,
+        textStyle: const TextStyle(color: Colors.white),
       ),
-      onPressed: () async {
-        await AuthService().signup(
-          email: _emailController.text,
-          password: _passwordController.text,
-          context: context,
-          role: _selectedRole,
-        );
-      },
-      child: const Text("Regístrate", style: TextStyle(color: Colors.white)),
+      onPressed: onPressed,
+      child: Text(text, style: const TextStyle(color: Colors.white)),
     );
   }
 
