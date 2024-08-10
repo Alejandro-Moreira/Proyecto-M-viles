b25b3be7b539b686b104facbb4b4a056f0581682
Jared Valenzuela <126533404+JaredVS777@users.noreply.github.com>
2024-08-10 15:04:56 -0500

    Login

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -4,9 +4,14 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Login extends StatelessWidget {
-  Login({super.key});
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
 
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -14,6 +19,12 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 50,
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
@@ -35,11 +46,11 @@ class Login extends StatelessWidget {
                     ),
                     const SizedBox(height: 8),
                     Text(
-                      'Login',
+                      'Iniciar sesión',
                       style: GoogleFonts.raleway(
                         textStyle: const TextStyle(
                           color: Colors.black,
-                          fontWeight: FontWeight.bold, // Cambiado a negrita
+                          fontWeight: FontWeight.bold,
                           fontSize: 24,
                         ),
                       ),
@@ -63,7 +74,7 @@ class Login extends StatelessWidget {
               const SizedBox(height: 30),
               _buildButton(
                 context: context,
-                text: 'Iniciar Sesión',
+                text: 'Iniciar sesión',
                 onPressed: () async {
                   await AuthService().signin(
                     email: _emailController.text,
@@ -72,24 +83,11 @@ class Login extends StatelessWidget {
                   );
                 },
               ),
-              const SizedBox(height: 10),
-              _buildButton(
-                context: context,
-                text: 'Registrarse',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Signup(),
-                    ),
-                  );
-                },
-                color: const Color(0xff1A1D1E),
-              ),
             ],
           ),
         ),
       ),
+      bottomNavigationBar: _signup(context),
     );
   }
 
@@ -139,20 +137,74 @@ class Login extends StatelessWidget {
     required BuildContext context,
     required String text,
     required VoidCallback onPressed,
-    Color color = const Color(0xff0D6EFD),
   }) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: color,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(14),
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 1, 75, 160),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
         ),
-        minimumSize: const Size(double.infinity, 60),
-        elevation: 0,
-        textStyle: const TextStyle(color: Colors.white),
+        child: Text(
+          text,
+          style: GoogleFonts.raleway(
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
       ),
-      onPressed: onPressed,
-      child: Text(text, style: const TextStyle(color: Colors.white)),
     );
   }
+
+  Widget _signup(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: const BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '¿No tienes una cuenta?',
+            style: GoogleFonts.raleway(
+              textStyle: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => const Signup(),
+                ),
+              );
+            },
+            child: Text(
+              'Regístrate',
+              style: GoogleFonts.raleway(
+                textStyle: const TextStyle(
+                  color: Color(0xffFF5C5C),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
